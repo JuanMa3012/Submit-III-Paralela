6c90172e02038f43c8f0fb58d73612f12025754c
Juan Manuel Hurtado Naranjo <106732762+JuanMa3012@users.noreply.github.com>
2025-09-21 17:43:01 -0500

    Update ex1.cpp

--- ex1.cpp
+++ ex1.cpp
@@ -11,37 +11,23 @@ int main(int argc, char* argv[]) {
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     const int N = 10000;
     vector<float> A(N), B(N);
-
     for (int i = 0; i < N; i++) {
         A[i] = (float)rank;
     }
-
     if (size != 2) {
         if (rank == 0) {
-            cout << "Este programa debe ejecutarse con 2 procesos." << endl;
-        }
+            cout << "Este programa debe ejecutarse con 2 procesos." << endl;   }
         MPI_Finalize();
         return 0;
     }
-
     int other = (rank == 0) ? 1 : 0;
-
-    MPI_Sendrecv(
-        A.data(), N, MPI_FLOAT, other, 0,
-        B.data(), N, MPI_FLOAT, other, 0,
-        MPI_COMM_WORLD, MPI_STATUS_IGNORE
-    );
-
+    MPI_Sendrecv(A.data(), N, MPI_FLOAT, other, 0, B.data(), N, MPI_FLOAT, other, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
     if (rank == 0) {
         cout << "Proceso " << rank << " recibió B[1234] = " << B[1234] << endl;
-
     }
-    
     else{
         cout << "Proceso " << rank << " recibió B[4321] = " << B[4321] << endl;
     }
-    
     MPI_Finalize();
-
     return 0;
 }
